1de9c5aa18b63a5a650c03dbf22fdd18e76eff61
Simon Gavira <sgavira@simons-mbp.lan>
2018-10-28 09:05:46 -0400

    added new route and query for automatic syndication

--- db/queries.js
+++ db/queries.js
@@ -10,6 +10,22 @@ function getEvents (req, res, next) {
     })
 }
 
-module.exports = { 
-    getEvents
+function newEvent (req, res, next) {
+  db
+    .none('INSERT INTO events(product_name, price, product_description)', {
+      product_name: req.body.product_name,
+      price: req.body.price,
+      product_description: req.body.product_description
+    })
+    .then(() => {
+      res.status(200).json({ status: 'Success' })
+    })
+    .catch(err => {
+      res.status(500).json({ status: `failed${err}` })
+    })
+}
+
+module.exports = {
+  getEvents,
+  newEvent
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express')
 var router = express.Router()
 const db = require('../db/queries')
 
-
 /* GET home page. */
 router.get('/getEvents', db.getEvents)
+router.post('/submitEvent', db.newEvent)
 
 module.exports = router
