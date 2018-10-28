65b4e77ac8bd0659a1bf604491e90ce68ddbd713
Simon Gavira <sgavira@simons-mbp.lan>
2018-10-28 07:58:08 -0400

    added frontend functionality from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,79 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+
+import axios from 'axios'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      eventName: '',
+      description: '',
+      price: 0
+    }
+  }
+
+  submit = () => {
+    const { eventName, description, price } = this.state
+    axios
+      .post('/submitEvent', {
+        product_name: eventName,
+        description: description,
+        price: price
+      })
+      .then(() => {
+        this.setState({
+          message: 'event submitted',
+          eventName: '',
+          description: '',
+          price: 0
+        })
+      })
+      .catch(err => {
+        this.setState({
+          message: err
+        })
+      })
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render () {
+    console.log(this.state)
     return (
       <div>
-        <input type='text' placeholder='Event Name ' /> <br />
-        <input type='text' placeholder='Description' /><br />
-        <input type='text' placeholder='Price' /><br />
+        <input
+          type='text'
+          placeholder='Event Name '
+          onChange={this.handleChange}
+          name='eventName'
+        />
+        {' '}
+        <br />
+        <input
+          type='text'
+          placeholder='Description'
+          onChange={this.handleChange}
+          name='description'
+        />
+        <br />
+        To the nearest cent:
+        <input
+          type='number'
+          min='0'
+          step='0.01'
+          data-number-to-fixed='2'
+          data-number-stepfactor='100'
+          class='currency'
+          onChange={this.handleChange}
+          name='price'
+        />
+        {' '}
+        <button onClick={this.submit}>Submit</button>
+        {this.state.message}
       </div>
     )
   }
