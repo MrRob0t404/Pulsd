f691d50bae18250ae38ae25ba2afaf12d7e6694e
Simon <simongaviria@pursuit.org>
2018-10-31 01:27:33 -0400

    added gitignore file

--- db/pulsd.sql
+++ db/pulsd.sql
@@ -11,9 +11,12 @@ CREATE TABLE events (
   product_name VARCHAR, 
   product_description VARCHAR,
   price Decimal,
+  startTime time without time zone,
+  endTime time without time zone, 
+  timezone VARCHAR,
   date_created timestamp DEFAULT (current_timestamp AT TIME ZONE 'EST')
 );
 
-INSERT INTO events (product_name, product_description, price)
-    VALUES('Party on the brooklyn bridge', 'Come have fun on the brooklyn bridge with some drink and friends', 100.00),
-    ('Party on the Williamsburg bridge', 'Come have fun on the Williamsburg bridge with some drink and friends', 200.00)
+INSERT INTO events (product_name, product_description, price, startTime, endTime, timezone)
+    VALUES('Party on the brooklyn bridge', 'Come have fun on the brooklyn bridge with some drink and friends', 100.00, '0405', '1405', 'America/New_York'),
+    ('Party on the Williamsburg bridge', 'Come have fun on the Williamsburg bridge with some drink and friends', 200.00, '0405', '1405', 'America/New_York')
